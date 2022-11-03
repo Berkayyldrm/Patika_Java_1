c107e01afec7c656487c8b2f89a3db3d858aff61
berkyyld <berkyyld@gmail.com>
2022-11-03 15:57:26 +0300

    Üs Hesabı Yapan Program

---
+++ Methods/Prime_Number_Recursive/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+    static void prime(int a){
+
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int number = input.nextInt();
+        prime(number);
+    }
+}
