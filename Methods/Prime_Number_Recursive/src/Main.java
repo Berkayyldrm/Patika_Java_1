b56057209ac4e942bfcafdd31cc8f59006242697
berkyyld <berkyyld@gmail.com>
2022-11-03 16:12:34 +0300

    Asal Sayı Bulan Program

--- Methods/Prime_Number_Recursive/src/Main.java
+++ Methods/Prime_Number_Recursive/src/Main.java
@@ -2,14 +2,22 @@ import java.util.Scanner;
 
 public class Main {
 
-    static void prime(int a){
-
+    static void prime(int a, int i){
+        if (a == i){
+            System.out.println("Prime Number");
+            return;
+        } else if (a%i == 0) {
+            System.out.println("Not Prime Number");
+            return;
+        }
+        prime(a, i + 1);
     }
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter a number : ");
         int number = input.nextInt();
-        prime(number);
+        prime(number, 2);
+
     }
 }
